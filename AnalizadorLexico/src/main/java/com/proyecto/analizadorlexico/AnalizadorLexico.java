58ac6b11edfd1779d1b9cf2bc8e14bafe2a94918
MarvinArch <marvinejo@gmail.com>
2023-08-08 23:04:15 -0600

    analizador lexico

---
+++ AnalizadorLexico/src/main/java/com/proyecto/analizadorlexico/AnalizadorLexico.java
@@ -0,0 +1,27 @@
+
+package com.proyecto.analizadorlexico;
+
+import com.proyecto.analizadorlexico.visual.VentanaInicial;
+
+/**
+ *
+ * @author david
+ */
+public class AnalizadorLexico {
+
+    public static void main(String[] args) {
+        
+        
+        
+        VentanaInicial init = new VentanaInicial();
+        init.setVisible(true);
+
+        try {
+
+        }catch (Exception e){
+            
+        }
+    }
+
+
+}
